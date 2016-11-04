3f25ac31703e028e28056b590d0a3c7c16add80d
adeutry <adeutry@temple.edu>
2016-11-04 18:00:44 -0400

    moved out of root home

---
+++ create_hit.py
@@ -0,0 +1,39 @@
+from boto.mturk.connection import MTurkConnection
+from boto.mturk.question import ExternalQuestion
+from boto.mturk.price import Price
+import sys
+
+AMAZON_HOST = "mechanicalturk.amazonaws.com"
+AMAZON_HOST_DEV = "mechanicalturk.sandbox.amazonaws.com"
+AMAZON_ACCESS_KEY_ID = "AKIAJYJ3K46RLMO6KA5Q"
+AMAZON_SECRET_ACCESS_KEY = "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"
+
+conn = MTurkConnection(aws_access_key_id=AMAZON_ACCESS_KEY_ID,
+                       aws_secret_access_key=AMAZON_SECRET_ACCESS_KEY,
+                       host=AMAZON_HOST)
+
+url = "https://mturk.adeutry.info"
+title = "Evaluate Translations"
+description = "Evaluate the performance of Machine Translation systems. Judge how fluent a sentence is."
+keywords = ["language", "translation"]
+frame_height = 650
+amount = 0.30
+
+question_form = ExternalQuestion(url, frame_height)
+
+# num_hits = 1 or int(iys.argv[1])
+
+if sys.argv[1] == "prod":
+    amazon_url = AMAZON_HOST 
+else if sys.argv[1] == "dev": 
+    amazon_url = AMAZON_HOST_DEV
+
+for i in range(1):
+        create_hit_result = conn.create_hit(
+                title = title,
+                description = description,
+                keywords = keywords,
+                max_assignments = 5,
+                question = question_form,
+                reward = Price(amount=amount)
+                )
---
+++ mturk_django/mturk/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+import pickle
+
+# Create your models here.
+class Sentence(models.Model):
+    text = models.CharField(max_length=1000)
+    slug = models.CharField(max_length=50)
+    grade_level = models.CharField(max_length=10)
+    align_id = models.IntegerField()
+
+class Translation(models.Model):
+    text = models.CharField(max_length=1000)
+    slug = models.CharField(max_length=50, default="")
+    grade_level = models.CharField(max_length=10, default="")
+    align_id = models.IntegerField(default=-1)
+    trans_text = models.CharField(max_length=1000)
+    trans_lang = models.CharField(max_length=10)
+    use_count = models.IntegerField(default=0)
+
+def save_sents_to_db():
+    with open("/root/python/newsela/data/pickles/aligned_sents_filtered.pick", "rb") as f:
+        sents = pickle.load(f)
+        for s in sents:
+            sent_obj = Sentence(
+                text = s['text'],
+                slug = s['slug'],
+                grade_level = s['grade_level'],
+                align_id = s['align_id']
+                    )
+            sent_obj.save()
+
+def save_trans_to_db():
+    with open("/root/python/newsela/data/pickles/aligned_sents_trans.pick", "rb") as f:
+        trans = pickle.load(f)
+        for t in trans:
+            trans_obj = Translation(
+               text = t['text'],
+               slug = t['slug'],
+               grade_level = t['grade_level'], 
+               align_id = t['align_id'],
+               trans_text = t['trans-text'],
+               trans_lang = t['trans-lang'],
+               use_count = 0
+            )
+            trans_obj.save()
