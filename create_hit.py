f3ca7da5a079802c33b55bd31247fd5626d2855b
adeutry <adeutry@temple.edu>
2016-11-07 14:52:18 -0500

    alternate dev site

--- create_hit.py
+++ create_hit.py
@@ -8,9 +8,6 @@ AMAZON_HOST_DEV = "mechanicalturk.sandbox.amazonaws.com"
 AMAZON_ACCESS_KEY_ID = "AKIAJYJ3K46RLMO6KA5Q"
 AMAZON_SECRET_ACCESS_KEY = "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"
 
-conn = MTurkConnection(aws_access_key_id=AMAZON_ACCESS_KEY_ID,
-                       aws_secret_access_key=AMAZON_SECRET_ACCESS_KEY,
-                       host=AMAZON_HOST)
 
 HIT_URL_PROD = "https://mturk.adeutry.info/fluency_1"
 HIT_URL_DEV =  "https://mturk.adeutry.info/fluency_1?dev=True"
@@ -21,16 +18,21 @@ keywords = ["language", "translation"]
 frame_height = 650
 amount = 0.30
 
-question_form = ExternalQuestion(url, frame_height)
 
 # num_hits = 1 or int(iys.argv[1])
 
 if sys.argv[1] == "prod":
     amazon_url = AMAZON_HOST 
     hit_url    = HIT_URL_PROD
-else if sys.argv[1] == "dev": 
+elif sys.argv[1] == "dev": 
     amazon_url = AMAZON_HOST_DEV
     hit_url    = HIT_URL_DEV
+
+question_form = ExternalQuestion(hit_url, frame_height)
+
+conn = MTurkConnection(aws_access_key_id=AMAZON_ACCESS_KEY_ID,
+                       aws_secret_access_key=AMAZON_SECRET_ACCESS_KEY,
+                       host=amazon_url)
 
 for i in range(1):
         create_hit_result = conn.create_hit(
