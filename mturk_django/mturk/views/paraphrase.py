59eac034608b5bb04c1a9112a2af0dae83512551
adeutry <adeutry@temple.edu>
2016-12-08 13:08:53 -0500

    added some cute buttons

--- mturk_django/mturk/views/paraphrase.py
+++ mturk_django/mturk/views/paraphrase.py
@@ -13,20 +13,11 @@ def index(request):
     sent = random.sample(list(sents), 1)[0]
     # prepare the context
     context = {
-            'worker_id': request.GET.get("workerId", "None"),
-            'assignment_id' : request.GET.get("assignmentId", "None"),
-            'amazon_host' : "https://www.mturk.com/mturk/externalSubmit",
             'in_dev' : request.GET.get("dev", "None"),
-            'hit_id' : request.GET.get("hitId", "None"),
+            'username' : request.GET.get("username", "None")
             }
 
     context['in_dev'] = (context['in_dev'] == "True")
-
-    # set external submit url
-    if context['in_dev']:
-        context['amazon_host'] = SUBMIT_URL_DEV
-    else:
-        context['amazon_host'] = SUBMIT_URL_PROD
 
     res = render(request, 'mturk/paraphrase.html', context)
     res['x-frame-options'] = 'do you like memes?'
@@ -34,8 +25,8 @@ def index(request):
 
 def get_rtt(request):
     text = request.GET.get("text", "None")
-    import utils.utils
-    rtt = utils.rtt(text, "de")
+    from .utils import utils
+    rtt = utils.rtt(text, "de", sleep=0.0)
     return HttpResponse(rtt)
 
 def get_paraphrase_sents(request):
@@ -45,7 +36,12 @@ def get_paraphrase_sents(request):
         grade_level='12.0'
     )
     
+    # serialize translations
     trans_json = [t.to_json() for t in trans]
+
+    # shuffle the translations
+    random.shuffle(trans_json)
+
     return HttpResponse(json.dumps(trans_json))
 
 
